16943b1f41c0ec90e0217e9920a5fa9f4becbe6a
matteovecchio04 <matteovex@gmail.com>
2025-10-07 13:44:31 +0200

    add comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,22 @@
 for (let i = 1; i <= 100; i++) {
-
+// for cycle that goes from 1 to 100
 if (i % 15 === 0) {
     console.log("FizzBuzz");
-    
+    // if the number is a multiple of 15, the console prints "FizzBuzz"
 }
 
 else if (i % 5 === 0) {
     console.log("Buzz");
-    
+    // if the number is a multiple of 5, the console prints "Buzz"
 }
 
 else if (i % 3 === 0) {
     console.log("Fizz");
-    
+    // if the number is a multiple of 3, the console prints "Fizz"
 }
 
 else {
     console.log(i);
-    
+    // if the number is NOT a multiple of 3,5 or 15, the console prints a number
 }
 }
